dd99a5eeca22011b708cd6794ef76733321bd048
NessaWaks <yjohn6717@gmail.com>
2025-07-22 18:02:04 +0100

    added icon

--- src/naviagationbar/NavigationBar.jsx
+++ src/naviagationbar/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './NavigationBar.css';
-import logo from '../assets/images/qmhr-logo.png';
+import logo from '../assets/images/qmhr-logo.png'; 
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
